ceaf5e1019d25bedd1e4ba4f55ed9d082ca887a6
alanzarate <alnzarate@gmail.com>
2023-04-21 10:18:56 -0400

    first commit

---
+++ src/main/kotlin/com/ucb/bo/sktmsgateway/SktMsGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.ucb.bo.sktmsgateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SktMsGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<SktMsGatewayApplication>(*args)
+}
---
+++ src/test/kotlin/com/ucb/bo/sktmsgateway/SktMsGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ucb.bo.sktmsgateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SktMsGatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
